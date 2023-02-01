94f33ccee8ea465b31da0758896769bb3cdc7baa
johangly <johanglyfabiansucre@gmail.com>
2023-01-31 21:54:51 -0400

    mediana feature complete

--- main.js
+++ main.js
@@ -33,17 +33,38 @@ agregarDatosButton.addEventListener('click',(e)=>{
 })
 calcularButton.addEventListener('click', ()=>{
   let tipo = tipoDeCalculo.value
-  if(tipo === 'promedio'){
-    let promedio = arrayDatos.reduce((current,value) =>{
-      return Number(current) + Number(value)
-    }) / arrayDatos.length;
-    mostrarResultado.textContent = `El promedio es ${promedio.toFixed(2)}`
-    return promedio;
-  }
-  if(tipo === 'mediana'){}
-  if(tipo === 'moda'){}
-});
+  if(arrayDatos.length > 0){
+    if(tipo === 'promedio'){
+      let promedio = arrayDatos.reduce((current,value) =>{
+        return Number(current) + Number(value)
+      }) / arrayDatos.length;
+      mostrarResultado.textContent = `El promedio es ${promedio.toFixed(2)}`
+      return promedio;
+    }
 
+    if(tipo === 'mediana'){
+      let arrayOrdenado = arrayDatos.sort((a,b)=>{
+        return Number(a) - Number(b);
+      })
+      if(arrayDatos.length > 1){
+        if(esPar(arrayDatos)){
+          let indexPromedio1 = arrayOrdenado[arrayOrdenado.length / 2];
+          let indexPromedio2 = arrayOrdenado[arrayOrdenado.length / 2 - 1];
+          let medianaPar = (Number(indexPromedio1) + Number(indexPromedio2)) / 2;
+          mostrarResultado.textContent = `La mediana es ${medianaPar.toFixed(2)}`
+          return medianaPar.toFixed(2);
+        }else{
+          let medianaInPar = Number(arrayOrdenado[(arrayOrdenado.length / 2) - 0.5]);
+          mostrarResultado.textContent = `La mediana es ${Number(medianaInPar).toFixed(2)}`;
+          return medianaInPar.toFixed(2);
+        }
+      }else{
+        mostrarResultado.textContent = `Se necesitas mas datos`;
+      }
+    }
+    if(tipo === 'moda'){}
+  }
+});
 const borrarDato=(e)=>{
   // elimina el elemento del documento html
   e.target.parentElement.remove()
@@ -53,4 +74,10 @@ const borrarDato=(e)=>{
   arrayDatos.splice(indexToDelete,1);
   console.log('calculo',value, indexToDelete,arrayDatos)
 }
-console.log()
+function esPar(arr){
+  if(arr.length % 2 === 0){
+    return true;
+  }else if(arr.length % 2 === 1){
+    return false;
+  }
+}
